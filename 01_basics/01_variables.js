84b18f9d34c38d94838216e51d13e85fbbe7e412
Shivani Choudhary <146834126+ShiviCoder@users.noreply.github.com>
2024-03-21 18:41:25 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+
+const accountId = '878728';
+var accountname = "rahul";
+let accountEmail = 'rahul@gmail.com'
+accountPassword = '34242';
+
+console.table([accountEmail,accountId,accountname,accountPassword]);
