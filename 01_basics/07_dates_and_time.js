39000fb06b523f6b04fec7881073f0f73e87ee93
Shivani Choudhary <146834126+ShiviCoder@users.noreply.github.com>
2024-03-23 12:53:50 +0000

    data and time in js

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,13 @@
+//Dates 
+
+let mydate  = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+//let myCreatedDate = new Date(2023,0,31)
+console.log(myCreatedDate);
+
+let myDate = new Date()
