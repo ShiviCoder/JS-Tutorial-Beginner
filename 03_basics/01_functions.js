0f1171a6028a54095b59651e3333fba2659f9fc4
Shivani Choudhary <146834126+ShiviCoder@users.noreply.github.com>
2024-03-26 15:46:41 +0000

    function in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log('S')
+    console.log('H')
+    console.log('I')
+    console.log('V')
+    console.log('A')
+    console.log('N')
+    console.log('I')
+}
+sayMyName //Reference
+sayMyName()  //print my name
+
+function addTwoNumber(num1 , num2) {
+    console.log()
+}
