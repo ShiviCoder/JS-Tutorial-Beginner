00d536379b5379d2c64017afd0f30772ea4a0b5c
Shivani Choudhary <146834126+ShiviCoder@users.noreply.github.com>
2024-03-27 08:08:16 +0000

    functions in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,9 +7,74 @@ function sayMyName(){
     console.log('N')
     console.log('I')
 }
-sayMyName //Reference
-sayMyName()  //print my name
+//sayMyName //Reference
+//sayMyName()  //print my name
 
-function addTwoNumber(num1 , num2) {
-    console.log()
+// function addTwoNumber(num1 , num2)//(parameters)
+//  {
+//     console.log(num1 + num2)
+// }
+
+function addTwoNumber(num1 , num2)//(parameters)
+ {
+    // let result = num1 + num2;
+    // return result
+    return num1 + num2;
 }
+// addTwoNumber()//arguments //Nan
+// addTwoNumber(3,4) //7
+// addTwoNumber(3,'e') //3e
+// addTwoNumber(4,null) //4
+
+const result = addTwoNumber(4,5)
+//console.log(result);  //9
+
+
+
+
+function registerUser(username = 'shivani'){ //shivani just logged in
+    if(!username){
+        console.log('please enter username');
+        return;
+    }
+    return `${username} just logged in`
+}
+//console.log(registerUser('choudhary')); //choudhary just logged in
+
+
+
+function calculateCartPrize(val1,val2,...num){
+    return num;
+}
+//console.log(calculateCartPrize(300,400,200,43000)) //[ 300, 400, 200, 43000 ]
+/*
+val1 => 300
+val2 => 400
+...num => 200,43000
+*/
+
+
+const user = {
+    name:'shivani',
+    prize: 1999
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.name} and prize is ${anyObject.prize}`)
+}
+//handleObject(user)
+
+// handleObject({
+//     name : 'sam',
+//     prize : 2000
+// })
+
+
+const myArray = [100,200,300,400]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myArray)); //200
+
+//console.log(returnSecondValue([10,20,30,40])); //20
